e33e9ba79b8ff5570ca2aa3c438c338349f55e38
Nate Jones <nate@endot.org>
2025-06-13 08:33:07 -0700

    Code done at first meetup, stopping before "Ingesting Content"

---
+++ dev/powerblog/dev.clj
@@ -0,0 +1,29 @@
+(ns powerblog.dev
+  (:require [powerblog.core :as blog]
+            [powerpack.dev :as dev]))
+
+(defmethod dev/configure! :default []
+  blog/config)  ;; 1
+
+(comment
+
+  (set! *print-namespace-maps* false)
+
+  (dev/start)
+  (dev/stop)
+  (dev/reset)
+
+  (def app (dev/get-app))
+
+  (require '[datomic.api :as d])
+
+  (def db (d/db (:datomic/conn app)))
+
+  (->> (d/entity db [:page/uri "/blog-posts/first-post/"])
+       :blog-post/author
+       (into {}))
+
+  ;;=> {:person/id :christian
+  ;;    :person/full-name "Christian Johansen"}
+
+  )
